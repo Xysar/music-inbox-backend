8ab45628ad6445cd5274ea09f2a5d21da45c51aa
Cesar Camacho <cesarcamacho760@gmail.com>
2023-06-07 17:30:50 -0700

    change column size for review content

--- src/main/java/com/example/music/inbox/backend/entity/Review.java
+++ src/main/java/com/example/music/inbox/backend/entity/Review.java
@@ -18,7 +18,7 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length=3000)
     private String content;
 
     @Column(nullable = false)
